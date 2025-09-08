a43d314bf27722650451987043b050843778a395
MIYATA026studyyyne <study.miyata026@gmail.com>
2025-09-08 23:59:17 +0900

    feat(logo): ロゴセクションを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <section id="logo" className="h-screen box-border p-4 bg-yellow-200">
+        <div>miyat-A-rchive</div>
+      </section>
       <section id="hero" className="box-border h-150 p-4 bg-blue-200">
         <div className="flex flex-col justify-center items-center h-full">
           <div className="relative w-80 h-80  bg-blue-500 rounded-full overflow-hidden">
