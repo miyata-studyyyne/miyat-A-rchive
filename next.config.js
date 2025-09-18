d956667c3ae662ddb6fd5f49958f6908f8a6b6c1
miyata-studyyyne <study.miyata026@gmail.com>
2025-09-19 03:54:11 +0900

    refactor(stack): Downgrade project stack to stable versions

---
+++ next.config.js
@@ -0,0 +1,5 @@
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {},
+};
+module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "var(--font-geist-sans)",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Noto Sans JP",
+          "Hiragino Sans",
+          "Hiragino Kaku Gothic ProN",
+          "Yu Gothic UI",
+          "Yu Gothic",
+          "Meiryo",
+          "sans-serif",
+        ],
+        mono: [
+          "var(--font-geist-mono)",
+          "ui-monospace",
+          "SFMono-Regular",
+          "monospace",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
